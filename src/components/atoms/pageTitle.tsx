1635c79d98a26988709c9b66a8fc3878de1594f5
杉田親次朗 <ba0534ps@icloud.com>
2019-12-17 00:29:30 +0900

    fix: menubar style

--- src/components/atoms/pageTitle.tsx
+++ src/components/atoms/pageTitle.tsx
@@ -13,9 +13,10 @@ const PageTitle: React.FC<Props> = ({ pageTitle, linkPath }) => (
 export default PageTitle;
 
 const StyledLink = styled(Link)`
-  font-size: 115%;
+  font-size: 150%;
   font-weight: bold;
+  font-family: 'Mongolian Baiti','sans-serif', 'Bodoni 72','Bodoni 72 Oldstyle';
   text-decoration: none;
-  color: #212121;
+  color: #a7a7a7;
   padding: 0 20px;
 `;
